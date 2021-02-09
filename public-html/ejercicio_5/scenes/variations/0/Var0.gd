443b56058ac7ba2c08c9bafb4d0945cb1f7752ae
dokasov <camilogozp@gmail.com>
2021-02-09 15:16:10 -0500

    ejercicio 5 cambiado

--- public-html/ejercicio_5/scenes/variations/0/Var0.gd
+++ public-html/ejercicio_5/scenes/variations/0/Var0.gd
@@ -10,10 +10,10 @@ var codigo ={
 	"m": "n", "n": "o", "o": "p", "p": "q",
 	"q": "r", "r": "s", "s": "t", "t": "u",
 	"u": "v", "v": "w", "w": "x", "x": "y",
-	"y": "z", "z": "a", " ": " ", "0": "[color=red]@[/color]",
-	"1": "[color=yellow]@[/color]", "2": "[color=blue]@[/color]", "3": "[color=green]@[/color]",
-	"4": "[color=purple]@[/color]", "5": "[color=orange]@[/color]", "6": "[color=white]@[/color]",
-	"7": "[color=black]@[/color]", "8": "[color=brown]@[/color]", "9": "[color=grey]@[/color]"
+	"y": "z", "z": "a", " ": " ", "0": "[color=red]■[/color]",
+	"1": "[color=yellow]■[/color]", "2": "[color=blue]■[/color]", "3": "[color=green]■[/color]",
+	"4": "[color=purple]■[/color]", "5": "[color=orange]■[/color]", "6": "[color=white]■[/color]",
+	"7": "[color=black]■[/color]", "8": "[color=brown]■[/color]", "9": "[color=grey]■[/color]"
 }
 
 func _ready():
@@ -25,19 +25,19 @@ func _on_LineEdit_text_changed(new_text):
 	_codificar()
 	
 func _codificar():
-	for i in codigo.keys():
-		if i == sentence[counter]:
-			answer.append(codigo[i])
-			var splitted = answer
-			var joined = PoolStringArray(splitted).join("")
-			$RichTextLabel.bbcode_text = str(joined)
-	counter += 1
+	answer = []
+	for letter in sentence:
+		for i in codigo.keys():
+			if i == letter:
+				answer.append(codigo[i])
+				var splitted = answer
+				var joined = PoolStringArray(splitted).join("")
+				$RichTextLabel.bbcode_text = str(joined)
 	
 #func _delete():
 #	if Input.is_action_pressed("borrar"):
 #		counter -= 1
 #		sentence.pop_back()
-		
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://scenes/variations/0/Var0.tscn")
