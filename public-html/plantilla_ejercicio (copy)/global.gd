d8c8ce0560eca34ab56ff0e0af11f8980a6f7329
AgustinVallejo <agustin.vallejo@udea.edu.co>
2021-02-11 11:53:12 -0500

    Deleted copy of plantilla

--- public-html/plantilla_ejercicio (copy)/global.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var answered_correctly
-var possible_variations = 1
-var variation_number
-
-func pick_random_variation_number():
-	randomize()
-	return randi() % possible_variations
