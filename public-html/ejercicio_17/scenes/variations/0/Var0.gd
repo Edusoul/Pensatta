96097af69c37805379d9a1c752198ce45f2973f0
AgustinVallejo <agustin.vallejo@udea.edu.co>
2021-02-10 20:57:12 -0500

    Fixes to 16 and Added Excercise 17

--- public-html/ejercicio_17/scenes/variations/0/Var0.gd
+++ public-html/ejercicio_17/scenes/variations/0/Var0.gd
@@ -1,18 +1,28 @@
 extends Node2D
 
-var pos1
-var pos2
-var pos3
+var in1
+var in2
+var in3
+
+
 
 func _ready():
+	$FinishedButton.hide()
 	pass
+	
+func _process(delta):
+	in1 = get_node("1stPlace").get("inside")
+	in2 = get_node("2stPlace").get("inside")
+	in3 = get_node("3stPlace").get("inside")
+	if in1 and in2 and in3:
+		$FinishedButton.show()
+	else:
+		$FinishedButton.hide()
 
 
 func _on_FinishedButton_pressed():
-	pos1 = $Shop1.get_position().y
-	pos2 = $Shop2.get_position().y
-	pos3 = $Shop3.get_position().y
-	
-	if (pos2 < pos3) and (pos3 < pos1):
-		global.answered_correctly = true
+	if 	get_node("1stPlace").get("shopNode") == $Shop2:
+		if 	get_node("2stPlace").get("shopNode") == $Shop3:
+			if 	get_node("3stPlace").get("shopNode") == $Shop1:
+				global.answered_correctly = true
 	get_tree().change_scene("res://scenes/End.tscn")
