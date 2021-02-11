5542ad0d71d3da7f2086ddee21931f96def63bf4
Wilson <wilsonrueda2@gmail.com>
2021-02-11 13:22:56 -0500

    refactoring exercise 8

--- public-html/ejercicio_8/scenes/End.gd
+++ public-html/ejercicio_8/scenes/End.gd
@@ -2,14 +2,14 @@ extends Control
 
 func _ready():
 	if global.answers.size() == 5 and global.answers_wrong.size() == 0:
-		$Label.visible = true
+		$Sprite/Label.visible = true
 	elif global.answers.size() == 4 and global.answers_wrong.size() == 1:
-		$Label2.visible = true
+		$Sprite/Label2.visible = true
 	elif global.answers.size() == 3 and global.answers_wrong.size() == 2:
-		$Label3.visible = true
+		$Sprite/Label3.visible = true
 	elif global.answers.size() == 2 and global.answers_wrong.size() == 3:
-		$Label4.visible = true
+		$Sprite/Label4.visible = true
 	elif global.answers.size() == 1 and global.answers_wrong.size() == 4:
-		$Label5.visible = true
+		$Sprite/Label5.visible = true
 	elif global.answers.size() == 0 and global.answers_wrong.size() == 5:
-		$Label6.visible = true
+		$Sprite/Label6.visible = true
