5db95f4f03cb0024a5fd1a5305757717549b6c44
creator-krixus <wilsonrueda2@gmail.com>
2021-04-22 12:56:09 -0500

    Final message missing

---
+++ Ejercicios Base 2/ejercicio_3/scenes/QuestionNode.gd
@@ -0,0 +1,28 @@
+extends Control
+
+export(String, MULTILINE) var question = "question"
+export var options = []
+export var correctIndex = []
+
+func _ready():
+	find_node("Question").text = question
+	
+	
+	var content = question
+	for option in options:
+		var answer = false
+		var button = Button.new()
+		button.text = option
+		button.toggle_mode = true
+		for i in correctIndex:
+			if option in options[i]:
+				answer = true		
+		button.connect("pressed", self, "_pressed",[answer])
+		get_node("PanelContainer/MarginContainer/GridContainer").add_child(button)
+
+func _pressed(rightAnswer):
+	yield(get_tree().create_timer(1.0), "timeout")
+	if rightAnswer:
+		pass #Qué se hace si la respuesta es correcta
+	else:
+		pass #Qué hacer si la respuesta fue incorrecta
